7928d9bce57786c28042dbe76b010efba9dc6381
Gal Gantar <gantar.gal@gmail.com>
2024-11-12 06:42:32 +0700

    Added proving and halo2 verifier

--- components/MainComponent/MainComponent.tsx
+++ components/MainComponent/MainComponent.tsx
@@ -1,7 +1,7 @@
-import { Button, Center, Code } from '@mantine/core';
-import { Prism } from '@mantine/prism';
+import { Button, Center, Code, Card, Text } from '@mantine/core';
+// import { Prism } from '@mantine/prism';
 import { Tabs, rem } from '@mantine/core';
-import { IconBoxModel, IconChartArrows, IconLoadBalancer } from '@tabler/icons-react';
+import { IconBoxModel, IconChartArrows, IconLoadBalancer, IconBrandGoogle, IconShield, IconMap } from '@tabler/icons-react';
 import {
     DynamicContextProvider,
     DynamicWidget,
@@ -18,8 +18,8 @@ import { useState } from 'react';
 export function MainComponent() {
     const iconStyle = { width: rem(24), height: rem(24) };
     const { primaryWallet } = useDynamicContext();
-    const [ verifierAddress, setVerifierAddress ] = useState(null);
-    const [ strategyAddress, setStrategyAddress ] = useState(null);
+    const [ verifierAddress, setVerifierAddress ] = useState("0xDfD349eC493C6afC77F859d00c8f03B36f9842b9");
+    const [ strategyAddress, setStrategyAddress ] = useState("0x2e293Bd3Bc02e83D3ef7794C4F64E4F1D1729Fb6");
 
     const codeLSTM = `# Define the LSTM model
 class LSTMModel(nn.Module):
@@ -58,12 +58,63 @@ class LSTMModel(nn.Module):
 
             <Tabs.Panel value="gallery">
                 <Center>
-                    <Prism language="python">{codeLSTM}</Prism>
+                    <p>LTSM model that predicts <b>upper tick</b> and <b>lower tick</b> for concentrated liquidity providing in Uniswap V3 clone. Because the model is compiled in a ZK circuit it <b>remanis private</b> to anyone else besides the developers.</p>
+                </Center>
+                <Center>
+                    <Code block mt={15} mb={15} style={{borderRadius: '5px'}}>{codeLSTM}</Code>
+                </Center>
+                <Center>
+                    <p>We compile the model into Zero-Knowledge circuit using <a href="https://github.com/zkonduit/ezkl" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: '#1c7ed6' }}>EZKL Library</a></p>.
+                </Center>
+                <Center mb={50}>
+                    <Button component="a" href="https://colab.research.google.com/drive/193KhBn4yOFwdHo2-7dXpq3S4Pk6oZD6R" target="_blank" rel="noopener noreferrer">
+                        Link to Google Colab
+                        <IconBrandGoogle style={{ marginLeft: '8px' }} />
+                    </Button>
                 </Center>
             </Tabs.Panel>
 
             <Tabs.Panel value="messages">
-                <Button onClick={
+                <Center>
+                    <p>We deployed two smart contracts on Criteria Testnet that allow autonomous agent to manage liquidity provisioning.</p>
+                </Center>
+                
+                <Center mt={20} mb={50}>
+                    <Card shadow="xl" padding="lg" style={{ margin: '10px', width: '45%' }}>
+                        <Card.Section>
+                            <Center>
+                                <IconShield size={48} />
+                            </Center>
+                        </Card.Section>
+                        <Text size="lg" mt="md" style={{fontWeight: 'bold'}}>
+                            Halo2 verifier contract
+                        </Text>
+                        <Text size="sm">
+                            Halo2 verifier contract that verifies that the resulting inference belongs to the given model. It was obtained by compiling LSTM model into ZK circuit.s  
+                        </Text>
+                        <Button component="a" href={`https://explorer.testnet.citrea.xyz/address/${verifierAddress}`} mt={20} target="_blank" rel="noopener noreferrer" style={{width: 'min-content', margin: 'auto'}}>
+                            View on Citrea Explorer
+                        </Button>
+                    </Card>
+                    <Card shadow="xl" padding="lg" style={{ margin: '10px', width: '45%' }}>
+                        <Card.Section>
+                            <Center>
+                                <IconMap size={48} />
+                            </Center>
+                        </Card.Section>
+                        <Text size="lg" mt="md" style={{fontWeight: 'bold'}}>
+                            Strategy contract
+                        </Text>
+                        <Text size="sm">
+                            Liquidity rebalancing strategy based on models predictions. Predictions are verified by <b>halo2 verifier contract</b> before being used.
+                        </Text>
+                        <Button component="a" href={`https://explorer.testnet.citrea.xyz/address/${strategyAddress}`} mt={20} target="_blank" rel="noopener noreferrer" style={{width: 'min-content', margin: 'auto'}}>
+                            View on Citrea Explorer
+                        </Button>
+                    </Card>
+                </Center>
+                
+                {/* <Button onClick={
                     async () => {
                         const signer = await primaryWallet?.connector.ethers?.getSigner();
                         const addr = await deployVerifier(signer);
@@ -83,10 +134,12 @@ class LSTMModel(nn.Module):
                         const verAddr = "0x1111111111111111111111111111111111111111";
                         deployStrategy(signer, verAddr!);
                     }
-                }>Deploy Strategy</Button>
+                }>Deploy Strategy</Button> */}
             </Tabs.Panel>
 
             <Tabs.Panel value="settings">
+                <p>Let's use the predictions of the private LSTM model. Firstly we <b>prove</b> that the predictions are valid outputs of LSTM model using <b>halo2 verifier</b>. Seconly we submit the predictions to the agent that utilizes them in its rebalancing strategy.</p>
+                
                 <Button onClick={
                     async () => {
                         const signer = await primaryWallet?.connector.ethers?.getSigner();
