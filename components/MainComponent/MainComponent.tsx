3069b3d67127ba70832f85d584fbf8ecb26b24fd
Gal Gantar <gantar.gal@gmail.com>
2024-11-12 05:32:21 +0700

    Updated frontend

--- components/MainComponent/MainComponent.tsx
+++ components/MainComponent/MainComponent.tsx
@@ -1,6 +1,7 @@
-import { Button, Center } from '@mantine/core';
+import { Button, Center, Code } from '@mantine/core';
+import { Prism } from '@mantine/prism';
 import { Tabs, rem } from '@mantine/core';
-import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react';
+import { IconBoxModel, IconChartArrows, IconLoadBalancer } from '@tabler/icons-react';
 import {
     DynamicContextProvider,
     DynamicWidget,
@@ -15,10 +16,26 @@ import { useState } from 'react';
 
 
 export function MainComponent() {
-    const iconStyle = { width: rem(12), height: rem(12) };
+    const iconStyle = { width: rem(24), height: rem(24) };
     const { primaryWallet } = useDynamicContext();
     const [ verifierAddress, setVerifierAddress ] = useState(null);
     const [ strategyAddress, setStrategyAddress ] = useState(null);
+
+    const codeLSTM = `# Define the LSTM model
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_layer_size, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+        self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size),
+                            torch.zeros(1, 1, self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq, self.hidden_cell)
+        predictions = self.linear(lstm_out[:, -1])
+        return predictions
+`;
 
     return (
     <div>
@@ -26,24 +43,23 @@ export function MainComponent() {
             <DynamicWidget />
         </Center>
         <Center>
-            <h1 style={{ fontSize: '40px' }}>Main Component</h1>
-        </Center>
-        <Center>
         <Tabs defaultValue="gallery" style={{width: "80%"}}>
             <Tabs.List style={{width: "100%", margin: 'auto', justifyContent: 'center'}}>
-                <Tabs.Tab value="gallery" leftSection={<IconPhoto style={iconStyle} />}>
-                Gallery
+                <Tabs.Tab value="gallery" leftSection={<IconBoxModel style={iconStyle} />} style={{fontSize: '20px'}}>
+                LSTM Model
                 </Tabs.Tab>
-                <Tabs.Tab value="messages" leftSection={<IconMessageCircle style={iconStyle} />}>
-                Messages
+                <Tabs.Tab value="messages" leftSection={<IconChartArrows style={iconStyle} />} style={{fontSize: '20px'}}>
+                Liquidity providing strategy
                 </Tabs.Tab>
-                <Tabs.Tab value="settings" leftSection={<IconSettings style={iconStyle} />}>
-                Settings
+                <Tabs.Tab value="settings" leftSection={<IconLoadBalancer style={iconStyle} />} style={{fontSize: '20px'}}>
+                Rebalancing with inference
                 </Tabs.Tab>
             </Tabs.List>
 
             <Tabs.Panel value="gallery">
-                Gallery tab content
+                <Center>
+                    <Prism language="python">{codeLSTM}</Prism>
+                </Center>
             </Tabs.Panel>
 
             <Tabs.Panel value="messages">
